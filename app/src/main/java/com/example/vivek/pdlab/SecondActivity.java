f6e9766ff4725da314131b8a61bcc39ddc61771a
vivek vicky <vivek vicky>
2017-03-22 23:20:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vivek/pdlab/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.vivek.pdlab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+}
