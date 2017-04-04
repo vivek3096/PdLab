aaac2698078ec66a8dac4d293fe0a1fe4222d358
Seetharaman-mobops <sri15496@gmail.com>
2017-04-05 01:43:21 +0530

    feat: added the bluetooth functionality

--- app/src/main/java/com/example/vivek/pdlab/SecondActivity.java
+++ app/src/main/java/com/example/vivek/pdlab/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.vivek.pdlab;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,8 +10,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,23 @@ public class SecondActivity extends AppCompatActivity {
         final ImageView ivLock2 = (ImageView)findViewById(R.id.iv_lock2);
         final SwitchCompat sc_lock1 = (SwitchCompat)findViewById(R.id.sc_lock1);
         SwitchCompat sc_lock2 = (SwitchCompat)findViewById(R.id.sc_lock2);
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+            Toast.makeText(this, "No bluetooth found, closing app...", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        else {
+
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+            else{
+                // todo pair with device and send data
+            }
+        }
 
         sc_lock1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
